ae4f364cf4b93204870f3ff74a1f01caa5c37d37
Andante <84418705+ksydoruk1508@users.noreply.github.com>
2024-11-08 13:27:51 +0200

    Update ICN_node.sh

--- ICN_node.sh
+++ ICN_node.sh
@@ -1,99 +1,103 @@
 #!/bin/bash
 
-function install_node {
-    echo "Updating and upgrading system packages..."
-    sudo apt-get update -y && sudo apt upgrade -y
-    echo "Installing dependencies..."
-    sudo apt-get install make screen build-essential unzip lz4 gcc git jq -y
+# Цвета текста
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m' # Нет цвета (сброс цвета)
 
-    echo "Installing Go..."
-    sudo rm -rf /usr/local/go
-    curl -Ls https://go.dev/dl/go1.22.4.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-    eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
-    eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+# Проверка наличия curl и установка, если не установлен
+if ! command -v curl &> /dev/null; then
+    sudo apt update
+    sudo apt install curl -y
+fi
+sleep 1
 
-    echo "Downloading project repository..."
-    wget https://github.com/hemilabs/heminetwork/releases/download/v0.5.0/heminetwork_v0.5.0_linux_amd64.tar.gz
-    tar -xvf heminetwork_v0.5.0_linux_amd64.tar.gz
-    rm -rf heminetwork_v0.5.0_linux_amd64.tar.gz
-    cd heminetwork_v0.5.0_linux_amd64/
+echo -e "${GREEN}"
+cat << "EOF"
+██ ███    ███ ██████   ██████  ███████ ███████ ██ ██████  ██      ███████      ██████ ██       ██████  ██    ██ ██████  
+██ ████  ████ ██   ██ ██    ██ ██      ██      ██ ██   ██ ██      ██          ██      ██      ██    ██ ██    ██ ██   ██ 
+██ ██ ████ ██ ██████  ██    ██ ███████ ███████ ██ ██████  ██      █████       ██      ██      ██    ██ ██    ██ ██   ██ 
+██ ██  ██  ██ ██      ██    ██      ██      ██ ██ ██   ██ ██      ██          ██      ██      ██    ██ ██    ██ ██   ██ 
+██ ██      ██ ██       ██████  ███████ ███████ ██ ██████  ███████ ███████      ██████ ███████  ██████   ██████  ██████  
+                                                                                                                        
+                                                                                                                        
+                        ███    ██ ███████ ████████ ██     ██  ██████  ██████  ██   ██                                   
+                        ████   ██ ██         ██    ██     ██ ██    ██ ██   ██ ██  ██                                    
+                        ██ ██  ██ █████      ██    ██  █  ██ ██    ██ ██████  █████                                     
+                        ██  ██ ██ ██         ██    ██ ███ ██ ██    ██ ██   ██ ██  ██                                    
+                        ██   ████ ███████    ██     ███ ███   ██████  ██   ██ ██   ██  
+                                    
+________________________________________________________________________________________________________________________________________
 
-    echo "Creating wallet..."
-    ./keygen -secp256k1 -json -net="testnet" > /root/heminetwork_v0.5.0_linux_amd64/popm-address.json
-    cat popm-address.json
-    echo "Save the above file and its data - this is your wallet!"
 
-    read -p "Enter your private key: " PRIVATE_KEY
-    echo "export POPM_PRIVATE_KEY=$PRIVATE_KEY" >> ~/.bashrc
-    echo "export POPM_STATIC_FEE=5000" >> ~/.bashrc
-    echo "export POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public" >> ~/.bashrc
-    source ~/.bashrc
+███████  ██████  ██████      ██   ██ ███████ ███████ ██████      ██ ████████     ████████ ██████   █████  ██████  ██ ███    ██  ██████  
+██      ██    ██ ██   ██     ██  ██  ██      ██      ██   ██     ██    ██           ██    ██   ██ ██   ██ ██   ██ ██ ████   ██ ██       
+█████   ██    ██ ██████      █████   █████   █████   ██████      ██    ██           ██    ██████  ███████ ██   ██ ██ ██ ██  ██ ██   ███ 
+██      ██    ██ ██   ██     ██  ██  ██      ██      ██          ██    ██           ██    ██   ██ ██   ██ ██   ██ ██ ██  ██ ██ ██    ██ 
+██       ██████  ██   ██     ██   ██ ███████ ███████ ██          ██    ██           ██    ██   ██ ██   ██ ██████  ██ ██   ████  ██████  
+                                                                                                                                         
+                                                                                                                                         
+ ██  ██████  ██       █████  ███    ██ ██████   █████  ███    ██ ████████ ███████                                                         
+██  ██        ██     ██   ██ ████   ██ ██   ██ ██   ██ ████   ██    ██    ██                                                             
+██  ██        ██     ███████ ██ ██  ██ ██   ██ ███████ ██ ██  ██    ██    █████                                                          
+██  ██        ██     ██   ██ ██  ██ ██ ██   ██ ██   ██ ██  ██ ██    ██    ██                                                             
+ ██  ██████  ██      ██   ██ ██   ████ ██████  ██   ██ ██   ████    ██    ███████
 
-    echo "Creating service file..."
-    sudo tee /etc/systemd/system/hemid.service > /dev/null <<EOF
-[Unit]
-Description=Hemi
-After=network.target
-
-[Service]
-User=$USER
-Environment="POPM_BTC_PRIVKEY=$POPM_PRIVATE_KEY"
-Environment="POPM_STATIC_FEE=5000"
-Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
-WorkingDirectory=/root/heminetwork_v0.5.0_linux_amd64
-ExecStart=/root/heminetwork_v0.5.0_linux_amd64/popmd
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
+Donate: 0x0004230c13c3890F34Bb9C9683b91f539E809000
 EOF
+echo -e "${NC}"
 
-    echo "Starting service..."
-    sudo systemctl enable hemid
-    sudo systemctl daemon-reload
-    sudo systemctl start hemid
-    echo "Node installation complete."
+function install_node {
+    echo -e "${BLUE}Обновляем сервер...${NC}"
+    sudo apt-get update -y && sudo apt upgrade -y && sudo apt-get install make curl -y
+
+    echo -e "${YELLOW}Введите private_key вашего EVM:${NC}"
+    read private_key_ICN
+
+    echo -e "${BLUE}Скачиваем и запускаем установочный скрипт ICN...${NC}"
+    nohup bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p ${private_key_ICN}" > icn_install.log 2>&1 &
+
+    echo -e "${GREEN}Установка ноды ICN запущена в фоновом режиме. Логи установки находятся в файле icn_install.log.${NC}"
 }
 
-function change_port {
-    read -p "Enter new port number: " NEW_PORT
-    sudo sed -i "s/Environment=\"POPM_BFG_URL=wss:\/\/testnet\.rpc\.hemi\.network\/v1\/ws\/public\"/Environment=\"POPM_BFG_URL=wss:\/\/testnet\.rpc\.hemi\.network:\$NEW_PORT\/v1\/ws\/public\"/g" /etc/systemd/system/hemid.service
-    sudo systemctl daemon-reload
-    sudo systemctl restart hemid
-    echo "Port changed to $NEW_PORT."
+function view_logs {
+    echo -e "${YELLOW}Просмотр логов установки (выход из логов CTRL+C)...${NC}"
+    tail -f icn_install.log
 }
 
 function remove_node {
-    echo "Stopping and disabling service..."
-    sudo systemctl stop hemid
-    sudo systemctl disable hemid
-    sudo rm /etc/systemd/system/hemid.service
-    sudo systemctl daemon-reload
-    echo "Removing node files..."
-    rm -rf /root/heminetwork_v0.5.0_linux_amd64
-    echo "Node removed successfully."
+    echo -e "${BLUE}Удаляем ноду ICN...${NC}"
+    pkill -f "curl -o- https://console.icn.global/downloads/install/start.sh"
+    echo -e "${GREEN}Процесс установки ноды ICN завершен.${NC}"
 }
 
-PS3="Please select an option: "
-options=("Install Node" "Change Port" "Remove Node" "Exit")
-select opt in "${options[@]}"; do
-    case $opt in
-        "Install Node")
-            install_node
-            ;;
-        "Change Port")
-            change_port
-            ;;
-        "Remove Node")
-            remove_node
-            ;;
-        "Exit")
-            break
-            ;;
-        *)
-            echo "Invalid option $REPLY"
-            ;;
-    esac
-done
+function main_menu {Видим, что статус “Node started successfully” — значит всё гудВидим, что статус “Node started successfully” — значит всё гуд
+
+
+Видим, что статус “Node started successfully” — значит всё гудВидим, что статус “Node started successfully” — значит всё гудВидим, что статус “Node started successfully” — значит всё гуд
+
+
+
+    while true; do
+        echo -e "${YELLOW}Выберите действие:${NC}"
+        echo -e "${CYAN}1. Установка ноды${NC}"
+        echo -e "${CYAN}2. Просмотр логов${NC}"
+        echo -e "${CYAN}3. Удаление ноды${NC}"
+        echo -e "${CYAN}4. Выход${NC}"
+       
+        echo -e "${YELLOW}Введите номер действия:${NC} "
+        read choice
+        case $choice in
+            1) install_node ;;
+            2) view_logs ;;
+            3) remove_node ;;
+            4) break ;;
+            *) echo -e "${RED}Неверный выбор, попробуйте снова.${NC}" ;;
+        esac
+    done
+}
+
+main_menu

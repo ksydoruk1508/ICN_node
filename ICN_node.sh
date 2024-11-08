bc252a2ef5d43cfe33c03def387e373271048aaf
Andante <84418705+ksydoruk1508@users.noreply.github.com>
2024-11-08 14:52:08 +0200

    Update ICN_node.sh

--- ICN_node.sh
+++ ICN_node.sh
@@ -63,7 +63,7 @@ function install_node {
     fi
 
     echo -e "${BLUE}Скачиваем и запускаем установочный скрипт ICN...${NC}"
-    nohup bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p ${private_key_ICN} " > icn_install.log 2>&1 &
+    nohup bash -c "curl -o- https://console.icn.global/downloads/install/start.sh | bash -s -- -p ${private_key_ICN} --monitoring.server.port ${monitoring_port} " > icn_install.log 2>&1 &
 
     echo -e "${GREEN}Установка ноды ICN запущена в фоновом режиме. Логи установки находятся в файле icn_install.log. Убедитесь, что порт доступен для использования.${NC}"
 }
